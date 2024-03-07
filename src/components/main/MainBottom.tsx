6bce1a08bcce51345a6814efb82bdcc53bbff7d0
yoshi9676 <yoshitarou0706@gmail.com>
2024-03-07 16:21:53 +0900

    messageの送信とfirestoreへの保存

--- src/components/main/MainBottom.tsx
+++ src/components/main/MainBottom.tsx
@@ -1,16 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import CardGiftcardIcon from "@mui/icons-material/CardGiftcard";
 import GifBoxIcon from "@mui/icons-material/GifBox";
 import SentimentVerySatisfiedSharpIcon from "@mui/icons-material/SentimentVerySatisfiedSharp";
+import {
+  CollectionReference,
+  DocumentData,
+  addDoc,
+  collection,
+  serverTimestamp,
+} from "@firebase/firestore";
+import { db } from "../../firebase";
+import { useAppSelector } from "../../app/hooks";
 
 function MainBottom() {
+  const [inputMessage, setInputMessage] = useState<string>("");
+  const channelId = useAppSelector((state) => state.channelState.channelId);
+  const user = useAppSelector((state) => state.userState.user);
+
+  const sendMessage = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    e.preventDefault();
+    if (!inputMessage || !channelId) {
+      alert("チャンネルを選択してください");
+      return;
+    }
+
+    const collectionRef: CollectionReference<DocumentData> = collection(
+      db,
+      "channels",
+      String(channelId),
+      "messages",
+    );
+
+    await addDoc(collectionRef, {
+      message: inputMessage,
+      timestamp: serverTimestamp(),
+      user,
+    });
+  };
+
   return (
     <div className="main-bottom">
       <AddCircleIcon />
       <form action="">
-        <input type="text" placeholder="Message" />
-        <button type="submit">送信</button>
+        <input
+          type="text"
+          placeholder="Message"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setInputMessage(e.target.value)
+          }
+        />
+        <button
+          type="submit"
+          onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>
+            sendMessage(e)
+          }
+        >
+          送信
+        </button>
       </form>
       <div className="main-bottom-icons">
         <CardGiftcardIcon />
